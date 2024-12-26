cde08bf8b6463ac07f77b375f690c8a99b9708ff
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2024-12-26 14:22:38 +0800

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Player from './components/Player.vue';
+import Sequence from './components/Sequence.vue';
+
+</script>
+
+<template>
+  <div>
+
+    <Player />
+    <Sequence />
+  </div>
+</template>
