84c7f6713f859193f126bbee2d57d4832f5422dd
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-04-17 19:12:07 +0800

    feat: 添加ImageDescriptor和ApplicationExtension类的解析功能

--- packages/gif/src/core/parsers/ImageDescriptor.ts
+++ packages/gif/src/core/parsers/ImageDescriptor.ts
@@ -1,7 +1,38 @@
+import { byteToBits, type IDataReader } from "../reader";
 import type { IParser } from "./parser";
 
 export class ImageDescriptor implements IParser {
+  left!: number
+  top!: number
+  width!: number
+  height!: number
+
+  localColorTableFlag!: number
+  interlaceFlag!: number
+  sortFlag!: number
+  sizeOfLocalColorTable!: number
+
+  constructor(private readonly reader: IDataReader) {
+    this.parse()
+  }
+
   parse(): void {
-    
+    this.left = this.reader.readUint16()
+    this.top = this.reader.readUint16()
+    this.width = this.reader.readUint16()
+    this.height = this.reader.readUint16()
+
+    const packed = this.reader.readByte()
+    this.packedField(packed)
+  }
+
+  packedField(byte: number) {
+    const bits = byteToBits(byte)
+    this.localColorTableFlag = bits[0]
+    this.interlaceFlag = bits[1]
+    this.sortFlag = bits[2]
+
+    const sizeCode = bits[5] * 4 + bits[6] * 2 + bits[7]
+    this.sizeOfLocalColorTable = 2 ** (sizeCode + 1)
   }
 }
--- packages/gif/src/core/parsers/applicationExtension.ts
+++ packages/gif/src/core/parsers/applicationExtension.ts
@@ -1,6 +1,87 @@
-import type { IParser } from "./parser";
+import type { IDataReader } from "../reader"
+import type { IParser } from "./parser"
+
+const NETSCAPE_IDENTIFIER = "NETSCAPE"
+const XMP_IDENTIFIER = "XMP DataXMP"
+
+export abstract class BasicExtension implements IParser {
+  identifier: string
+  authCode: string
+  blockSize: number
+
+  constructor(
+    protected readonly reader: IDataReader,
+    identifier: string,
+    authCode: string,
+    blockSize: number
+  ) {
+    this.identifier = identifier
+    this.authCode = authCode
+    this.blockSize = blockSize
+  }
+
+  abstract parse(): void
+}
+
+export class NetscapeExtension extends BasicExtension {
+  loopCount: number | null = null
+
+  parse(): void {
+    const subBlock = this.reader.readSubBlocks()
+    if (subBlock.length >= 3 && subBlock[0] === 1) {
+      this.loopCount = subBlock[1] + subBlock[2] * 256
+    }
+  }
+
+  isVaildIdentifer(): boolean {
+    return this.identifier.startsWith(NETSCAPE_IDENTIFIER)
+  }
+}
+
+class XMPExtension extends BasicExtension {
+  data!: Uint8Array
+
+  parse(): void {
+    this.data = this.reader.readSubBlocks()
+  }
+
+  isVaildIdentifer(): boolean {
+    return this.identifier === XMP_IDENTIFIER
+  }
+}
+
+export class UnknownExtension extends BasicExtension {
+  data!: Uint8Array
+
+  parse(): void {
+    this.data = this.reader.readSubBlocks()
+  }
+}
+
 
 export class ApplicationExtension implements IParser {
+  extension!: BasicExtension
+
+  constructor(private readonly reader: IDataReader) {
+    this.parse()
+  }
+
   parse(): void {
+    const blockSize = this.reader.readByte()
+    const appIdBytes = this.reader.readBytes(8)
+    const authBytes = this.reader.readBytes(3)
+
+    const identifier = String.fromCharCode(...appIdBytes)
+    const authCode = String.fromCharCode(...authBytes)
+
+    if (identifier.startsWith(NETSCAPE_IDENTIFIER)) {
+      this.extension = new NetscapeExtension(this.reader, identifier, authCode, blockSize)
+    } else if (identifier === XMP_IDENTIFIER) {
+      this.extension = new XMPExtension(this.reader, identifier, authCode, blockSize)
+    } else {
+      this.extension = new UnknownExtension(this.reader, identifier, authCode, blockSize)
+    }
+
+    this.extension.parse()
   }
 }
